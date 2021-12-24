dc919d13204c2f02cabae948a21f9fda3182098a
Michelli <contato@michellicristina.com.br>
2021-12-24 16:23:30 -0300

    chore: confira projeto + configura launchscreen + configura início main

---
+++ imc-appUITests/imc_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  imc_appUITestsLaunchTests.swift
+//  imc-appUITests
+//
+//  Created by Michelli Cristina de Paulo Lima on 24/12/21.
+//
+
+import XCTest
+
+class imc_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
